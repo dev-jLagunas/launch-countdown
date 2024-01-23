7c5317245349135daf9fc4767f7c1e44e8093840
Juan Lagunas <dev.jlagunas@gmail.com>
2024-01-23 14:47:26 +0900

    Complete change date functionality. Start refactoring to prepare for deployment

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 import "./style.scss";
 
-window.handleTickInit = function (tick) {
-  var countdown = Tick.count.down("2024-04-23T00:00:00");
+let tick;
+let countdown;
 
+const dateInput = document.querySelector('input[type="date"]');
+const submitButton = document.querySelector('button[type="submit"]');
+
+// Set the initial value of the date input to the initial countdown date
+dateInput.value = "2024-04-23";
+
+window.handleTickInit = function (tickInstance) {
+  tick = tickInstance;
+  startCountdown(dateInput.value + "T00:00:00");
+};
+
+function startCountdown(date) {
+  // If a countdown already exists, remove the update event listener
+  if (countdown) {
+    countdown.onupdate = function () {};
+  }
+
+  // Start a new countdown
+  countdown = Tick.count.down(date);
   countdown.onupdate = function (value) {
     tick.value = value;
   };
+}
 
-  countdown.onended = function () {
-    // Handle when the countdown ends
-  };
-};
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  // Create a new tick instance with the updated countdown date
+  const newDate = dateInput.value + "T00:00:00";
+  startCountdown(newDate);
+});
